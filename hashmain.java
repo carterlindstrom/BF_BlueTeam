baefa23b7cb35dd689dc282dafee060338b435f6
ejkutz <79886893+ejkutz@users.noreply.github.com>
2021-03-04 13:13:22 -0600

    Add files via upload

--- hashmain.java
+++ hashmain.java
@@ -30,6 +30,11 @@ public class hashmain {
     testback.addAvgRating("2");
     testback.addAvgRating("1");
     testback.addAvgRating("0");
+    List<String> list2= testback.getAllGenres();
+    for (int x=0; x< list2.size();x++)
+    {
+      testback.addGenre(list2.get(x));
+    }
     printList();
     
    }
@@ -48,7 +53,7 @@ public class hashmain {
       for (int x=0; x< testback.getNumberOfMovies();x++)
       {
         if(list.get(x)!=null)
-          System.out.print(x+1 + " " + list.get(x));
+          System.out.println(x+1 + " " + list.get(x).getTitle());
           
       }
       String input = scnn.nextLine();
@@ -61,13 +66,13 @@ public class hashmain {
       else if (input.equals("r")||input.equals("R")) {
         changeRating();
       }
-      else if (list.get(1)!=null&&(input.equals("1")||input.equals("One")||input.equals("one"))) {
+      else if (list.get(0)!=null&&(input.equals("1")||input.equals("One")||input.equals("one"))) {
         displayMovie(list.get(0));
       }
-      else if (list.get(2)!=null&&(input.equals("2")||input.equals("two")||input.equals("Two"))) {
+      else if (list.get(1)!=null&&(input.equals("2")||input.equals("two")||input.equals("Two"))) {
         displayMovie(list.get(1));
       }
-      else if (list.get(3)!=null&&(input.equals("3")||input.equals("Three")||input.equals("three"))) {
+      else if (list.get(2)!=null&&(input.equals("3")||input.equals("Three")||input.equals("three"))) {
         displayMovie(list.get(2));
       }
     }
@@ -85,35 +90,39 @@ public class hashmain {
       System.out.println("Genres:");
       for (int x=0; x< list2.size();x++)
       {
+        boolean pass=true;
           for(int y=0; y<list1.size();y++)
-            if(list1.get(x).equals(list2.get(y)))
+            if(list1.get(y).equals(list2.get(x))) {
+              pass=false;
               break;
-            else if(list1.size()-1==x)
-              list3.add(list2.get(x));
+            }
+          if (pass)
+           list3.add(list2.get(x));
           
       }
       System.out.println("Genres currently being sorted ");
       for (int x=0; x< list1.size();x++)
       {
-          System.out.print(x+1 + " " + list1.get(x));
-          lengthlist=x;
+          System.out.println(x+1 + " Genre:" + list1.get(x));
+          lengthlist=x+1;
       }
       System.out.println("Genres not included ");
       for (int x=0; x< list3.size();x++)
       {
-          System.out.print(x+1+lengthlist + " " + list3.get(x));
+          System.out.println(x+1+lengthlist + " Genre:" + list3.get(x));
       }
       String input = scnn.nextLine();
       if (input.equals("x")||input.equals("X")) {
         printList();
       }
-      else try {
-        inputnum = Double.parseDouble(input);
+      else {
+      try {
+        inputnum = Integer.parseInt(input);
     } catch (NumberFormatException nfe) {
       System.out.println("Improper input, try again.");
-      changeGenre();
+      changeRating();
     }
-      if (inputnum-1 < list2.size()&&inputnum-1 >=0) {
+      if (inputnum-1 > list2.size()||inputnum-1 <0) {
         System.out.println("Improper input, try again.");
         changeGenre();
       }
@@ -121,10 +130,14 @@ public class hashmain {
       if (inputnum-1 < lengthlist) {
         System.out.println("Removing "+ list1.get((int) (inputnum-1)));
         testback.removeGenre(list1.get((int) (inputnum-1)));
+        changeGenre();
+        
       }
-      else if (inputnum-1 > lengthlist) {
-        System.out.println("Removing "+ list3.get((int) (inputnum-1-lengthlist)));
-        testback.addGenre(list3.get((int) (inputnum-1)));
+      else if (inputnum-1 >= lengthlist) {
+        System.out.println("Adding "+ list3.get((int) (inputnum-1-lengthlist)));
+        testback.addGenre(list3.get((int) (inputnum-1-lengthlist)));
+        changeGenre();
+      }
       }
     }
     public static void changeRating() {
@@ -132,6 +145,7 @@ public class hashmain {
       double inputnum=0;
       List<String> list1= testback.getAvgRatings();
     List<String> list2= new ArrayList<String>();
+    
     list2.add("0");
     list2.add("1");
     list2.add("2");
@@ -150,46 +164,54 @@ public class hashmain {
       System.out.println("Average Ratings:");
       for (int x=0; x< list2.size();x++)
       {
+        boolean pass=true;
           for(int y=0; y<list1.size();y++)
-            if(list1.get(x).equals(list2.get(y)))
+            if(list1.get(y).equals(list2.get(x))) {
+              pass=false;
               break;
-            else if(list1.size()-1==x)
-              list3.add(list2.get(x));
+            }
+          if (pass)
+           list3.add(list2.get(x));
           
       }
       System.out.println("Ratings currently being sorted ");
       for (int x=0; x< list1.size();x++)
       {
-          System.out.print(x+1 + " " + list1.get(x));
-          lengthlist=x;
+          System.out.println(x+1 + " Rating:" + list1.get(x));
+          lengthlist=x+1;
       }
       System.out.println("Ratings not included ");
       for (int x=0; x< list3.size();x++)
       {
-          System.out.print(x+1+lengthlist + " " + list3.get(x));
+          System.out.println(x+1+lengthlist + " Rating:" + list3.get(x));
       }
       String input = scnn.nextLine();
       if (input.equals("x")||input.equals("X")) {
         printList();
       }
-      else try {
-        inputnum = Double.parseDouble(input);
+      else {
+      try {
+        inputnum = Integer.parseInt(input);
     } catch (NumberFormatException nfe) {
       System.out.println("Improper input, try again.");
-      changeGenre();
+      changeRating();
     }
-      if (inputnum-1 < list2.size()&&inputnum-1 >=0) {
+      if (inputnum-1 > list2.size()||inputnum-1 <0) {
         System.out.println("Improper input, try again.");
-        changeGenre();
+        changeRating();
       }
        
       if (inputnum-1 < lengthlist) {
         System.out.println("Removing "+ list1.get((int) (inputnum-1)));
-        testback.removeGenre(list1.get((int) (inputnum-1)));
+        testback.removeAvgRating(list1.get((int) (inputnum-1)));
+        changeRating();
+        
       }
-      else if (inputnum-1 > lengthlist) {
+      else if (inputnum-1 >= lengthlist) {
         System.out.println("Removing "+ list3.get((int) (inputnum-1-lengthlist)));
-        testback.addGenre(list3.get((int) (inputnum-1)));
+        testback.addAvgRating(list3.get((int) (inputnum-1)));
+        changeRating();
+      }
       }
     }
       
@@ -197,7 +219,7 @@ public class hashmain {
       //clearScreen();
       List<String> list1= movie.getGenres();
       System.out.println("The movie title is: " + movie.getTitle());
-      System.out.println("genres: " + movie.getDirector());
+      System.out.println("genres: ");
       for (int x=0; x< list1.size();x++)
       {
         System.out.println(list1.get(x));
@@ -207,6 +229,7 @@ public class hashmain {
       System.out.println("It was directed by: " + movie.getDirector());
       System.out.println("Press any key to go back ");
       String input = scnn.nextLine();
+      printList();
       
     }
 
