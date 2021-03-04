90ac1a30d935c86b3379da69cd89fa557bf543b9
ejkutz <79886893+ejkutz@users.noreply.github.com>
2021-03-04 16:40:57 -0600

    Add files via upload

--- Junittest.java
+++ Junittest.java
@@ -13,13 +13,13 @@ public class Junittest {
   @Test
   void givenSystemOutRedirection_whenInvokePrintln_thenOutputCaptorSuccess() {
        hashmain.main(null);
-      Assert.assertEquals("elcome to the Movie Sorter program\r\n"
+      Assert.assertEquals("Welcome to the Movie Sorter program\r\n"
           + "Please enter the number of the movie to recieve more details\r\n"
           + "Or, press G to enter genre selection or R to enter Rating selection\r\n"
           + "X will exit the program\r\n"
           + "Movies:\r\n"
-          + "1 title3\r\n"
-          + "2 title1\r\n"
-          + "3 title2\r\n", outputStreamCaptor.toString().trim());
+          + "1 The Moving on Phase\r\n"
+          + "2 As I Am\r\n"
+          + "3 Breakout\r\n", outputStreamCaptor.toString().trim());
   }
   }*/
